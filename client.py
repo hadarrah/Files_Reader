0b59d5014900a3d000612b1b38a3b4a7287ba2bf
Hadar <hadar.rahamim91@gmail.com>
2019-11-22 12:54:52 +0200

    Change models packages

--- client.py
+++ client.py
@@ -1,11 +1,19 @@
-import logging
+from Utils import logger, flow_parser
 import socket
+import json
+import time
+import random
+
+NUMBER_OF_FILES_PER_REQUEST = 10
+INTERVAL = 5
+
 
 class Client():
     def __init__(self, address, port):
         self.address = address
         self.port = port
-        self.logger = logging.getLogger(__name__)
+        self.counter = 0
+        self.logger = logger.get_logger(__name__)
 
     def run(self):
         # Create a TCP/IP socket
@@ -16,13 +24,35 @@ class Client():
         self.logger.info('connecting to %s port %s' % server_address)
         sock.connect(server_address)
 
+        try:
+            while True:
+                files_array = self.randomized_json_files()
 
-        while True:
-            try:
                 # Send data
-                message = 'This is the message.  It will be repeated.'
-                sock.sendall(message)
+                msg = json.dumps(files_array).encode()
+                sock.sendall(msg)
+                time.sleep(INTERVAL)
+        finally:
+            sock.close()
+
+    def randomized_json_files(self):
+        array_to_send = {}
+        for i in range(NUMBER_OF_FILES_PER_REQUEST):
+            file_name = "file_{idx}".format(idx=self.counter)
+            rand_num = random.randint(1, 10)
+            if rand_num == 1:
+                array_to_send[file_name] = "corrupt"
+            else:
+                array_to_send[file_name] = "not-corrupt"
+            self.counter += 1
+        return array_to_send
 
 
-            finally:
-                sock.close()
+if __name__ == '__main__':
+    args = flow_parser.parse_arguments()
+
+    logger.setup()
+    log = logger.get_logger(__name__)
+    log.info("Starting Files Reader Client Process")
+
+    Client(args.address, args.port).run()
--- server.py
+++ server.py
@@ -1,13 +1,49 @@
-import logging
+from Utils import logger, flow_parser
 import socket
+import parser
+import json
+import psycopg2
+
 
 class Server():
-    def __init__(self, address, port):
+    def __init__(self, address, port, db_host, db_name, db_usr, db_pass):
         self.address = address
         self.port = port
-        self.logger = logging.getLogger(__name__)
+        self.db_host = db_host
+        self.db_name = db_name
+        self.db_usr = db_usr
+        self.db_pass = db_pass
+        self.logger = logger.get_logger(__name__)
 
     def run(self):
+        sock = self.create_sock_connection()
+        db_cur, db_con = self.create_db_connection()
+
+        while True:
+            # Wait for a connection
+            self.logger.info('waiting for a connection')
+            connection, client_address = sock.accept()
+            try:
+                self.logger.info('connection from ' + str(client_address))
+
+                # Receive the data in small chunks and retransmit it
+                while True:
+                    data = connection.recv(512).decode()
+
+                    if data:
+                        files_array = json.loads(data)
+                        for file in files_array:
+                            self.logger.info('received file {file} with status {status}'.
+                                             format(file=file, status=str(files_array[file])))
+                            db_cur.execute("INSERT INTO files_reader Values ('{name}', '{status}');".format(name=file, status=files_array[file]))
+                            db_con.commit()
+
+            finally:
+                # Clean up the connection
+                db_cur.close()
+                db_con.close()
+
+    def create_sock_connection(self):
         # Create a TCP/IP socket
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -19,20 +55,19 @@ class Server():
         # Listen for incoming connections
         sock.listen(1)
 
-        while True:
-            # Wait for a connection
-            self.logger.info('waiting for a connection')
-            connection, client_address = sock.accept()
-            try:
-                self.logger.info('connection from ' + client_address)
+        return sock
 
-                # Receive the data in small chunks and retransmit it
-                while True:
-                    data = connection.recv(16)
-                    self.logger.info('received "%s"' % data)
-                    if data:
-                        pass
+    def create_db_connection(self):
+        conn = psycopg2.connect(host=self.db_host, database=self.db_name, user=self.db_usr, password=self.db_pass)
+        cur = conn.cursor()
+        return cur, conn
 
-            finally:
-                # Clean up the connection
-                connection.close()
+
+if __name__ == '__main__':
+    args = flow_parser.parse_arguments()
+
+    logger.setup()
+    log = logger.get_logger(__name__)
+    log.info("Starting Files Reader Server Process")
+
+    Server(args.address, args.port, args.db_host, args.db_name, args.db_user, args.db_password).run()
